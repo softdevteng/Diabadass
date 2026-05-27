9ff550aa28504aba5f950b83642cb71ddf1f0fe4
Your Name <your-email@example.com>
2026-05-27 10:59:16 +0300

    Gallery: use local photos, add credit; add GitHub Pages deploy workflow

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -1,3 +1,23 @@
-const images = ["/photos/IMG-20260527-WA0012.jpg","/photos/IMG-20260527-WA0014.jpg","/photos/IMG-20260527-WA0015.jpg","/photos/IMG-20260527-WA0017.jpg","/photos/IMG-20260527-WA0018.jpg","/photos/IMG-20260527-WA0019.jpg","/photos/IMG-20260527-WA0020.jpg","/photos/IMG-20260527-WA0021.jpg","/photos/IMG-20260527-WA0022.jpg","/photos/IMG-20260527-WA0023.jpg","/photos/IMG-20260527-WA0024.jpg","/photos/IMG-20260527-WA0025.jpg","/photos/IMG-20260527-WA0027.jpg","/photos/IMG-20260527-WA0028.jpg","/photos/IMG-20260527-WA0029.jpg"]
+
+const images = [
+	"/photos/IMG-20260527-WA0012.jpg",
+	"/photos/IMG-20260527-WA0014.jpg",
+	"/photos/IMG-20260527-WA0015.jpg",
+	"/photos/IMG-20260527-WA0017.jpg",
+	"/photos/IMG-20260527-WA0018.jpg",
+	"/photos/IMG-20260527-WA0019.jpg",
+	"/photos/IMG-20260527-WA0020.jpg",
+	"/photos/IMG-20260527-WA0021.jpg",
+	"/photos/IMG-20260527-WA0022.jpg",
+	"/photos/IMG-20260527-WA0023.jpg",
+	"/photos/IMG-20260527-WA0024.jpg",
+	"/photos/IMG-20260527-WA0025.jpg",
+	"/photos/IMG-20260527-WA0027.jpg",
+	"/photos/IMG-20260527-WA0028.jpg",
+	"/photos/IMG-20260527-WA0029.jpg",
+]
 
 export default images
+
+// Small friendly credit for the page
+export const credit = 'Made with love by Mwangi❤️'
