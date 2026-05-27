a7b30a72ce4cc79552980fb1134eb4afc1d9959a
Your Name <your-email@example.com>
2026-05-27 10:53:59 +0300

    Update galleryImages.js with local photos

--- src/data/galleryImages.js
+++ src/data/galleryImages.js
@@ -1,3 +1,3 @@
-const images = 
+const images = ["/photos/IMG-20260527-WA0012.jpg","/photos/IMG-20260527-WA0014.jpg","/photos/IMG-20260527-WA0015.jpg","/photos/IMG-20260527-WA0017.jpg","/photos/IMG-20260527-WA0018.jpg","/photos/IMG-20260527-WA0019.jpg","/photos/IMG-20260527-WA0020.jpg","/photos/IMG-20260527-WA0021.jpg","/photos/IMG-20260527-WA0022.jpg","/photos/IMG-20260527-WA0023.jpg","/photos/IMG-20260527-WA0024.jpg","/photos/IMG-20260527-WA0025.jpg","/photos/IMG-20260527-WA0027.jpg","/photos/IMG-20260527-WA0028.jpg","/photos/IMG-20260527-WA0029.jpg"]
 
 export default images
